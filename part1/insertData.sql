86c2568fc187f3eb06e1a888bab9dff892b19008
yosep <a1939679@student.edu.au>
2025-06-20 13:44:32 +0000

    [automatic commit] 2025-06-20 13:44:32 UTC

--- part1/insertData.sql
+++ part1/insertData.sql
@@ -2,8 +2,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('gurtyo', 'dave@example.com', 'hashed679', 'walker'),
-('emilyowner', 'emily@example.com', 'hashed321', 'owner');
+('gurtyo', 'gurt@example.com', 'hashed679', 'walker'),
+('sigm', 'emily@example.com', 'hashed321', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
