680dc3d4d6c93e1b00a0e5dbcae78a89dfd22565
yosep <a1939679@student.edu.au>
2025-06-20 14:28:03 +0000

    [automatic commit] 2025-06-20 14:28:03 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -43,6 +43,16 @@ router.get('/api/walkrequests/open', function(req, res, next) {
         res.status(500).json({ error: 'Database connection error'});
         return;
       }
+
+      const query = `
+        SELECT WalkRequests.request_id, Dogs.name AS dog_name,
+               WalkRequests.requested_time, WalkRequests.duration_minutes,
+               WalkRequests.location, Users.username AS owner_username
+        FROM WalkRequests
+        JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+        JOIN Users ON Dogs.owner_id = Users.user_id
+        WHERE WalkRequests.status = 'open'
+      `;
     });
   }
 });
