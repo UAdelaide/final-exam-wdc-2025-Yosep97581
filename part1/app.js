cd7e2357a2ce5e1985515850479453d622095d16
yosep <a1939679@student.edu.au>
2025-06-21 01:54:33 +0000

    [automatic commit] 2025-06-21 01:54:33 UTC

--- part1/app.js
+++ part1/app.js
@@ -22,7 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(session)
+app.use(session ({
+  secret: 'dog'
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
