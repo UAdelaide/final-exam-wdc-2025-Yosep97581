a798f2a97b43f61b3027c87b519e92ac2fded671
yosep <a1939679@student.edu.au>
2025-06-21 01:55:53 +0000

    [automatic commit] 2025-06-21 01:55:53 UTC

--- part1/app.js
+++ part1/app.js
@@ -19,14 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(session ({
   secret: 'secret-key',
   resave: false,
   saveUninitialized: true
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
