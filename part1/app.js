cedfa6c019e4cdd2f085c22f5bcb884afb1dd2ff
yosep <a1939679@student.edu.au>
2025-06-21 01:56:13 +0000

    [automatic commit] 2025-06-21 01:56:13 UTC

--- part1/app.js
+++ part1/app.js
@@ -19,11 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session ({
+app.use(session({
   secret: 'secret-key',
   resave: false,
   saveUninitialized: true
 }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
