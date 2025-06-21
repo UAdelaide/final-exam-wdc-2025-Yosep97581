d9a9bab71e6c18b3409c5baff0b698e41e5507bf
yosep <a1939679@student.edu.au>
2025-06-21 04:58:13 +0000

    [automatic commit] 2025-06-21 04:58:13 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,6 +59,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//
 router.post('/logout', (req, res) => {
   req.session.destory((err) => {
     if (err) return res.status(500).json({ error: 'Logout failed' });
