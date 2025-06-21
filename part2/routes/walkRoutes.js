0ce8a882e6edb82a91ddb87f9b819f2605065173
yosep <a1939679@student.edu.au>
2025-06-21 05:07:21 +0000

    [automatic commit] 2025-06-21 05:07:21 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -59,6 +59,7 @@ router.post('/:id/apply', async (req, res) => {
   }
 });
 
+// 
 router.get('/listdogs', async (req, res) => {
   if (!req.session.user || req.session.user.role !== 'owner') {
     return res.status(401).json({ error: 'Unauthorised' });
