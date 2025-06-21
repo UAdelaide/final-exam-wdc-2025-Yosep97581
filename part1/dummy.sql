ede58adede35562ce0181eb6cf7d693417374aa5
yosep <a1939679@student.edu.au>
2025-06-21 03:31:08 +0000

    [automatic commit] 2025-06-21 03:31:08 UTC

--- part1/dummy.sql
+++ part1/dummy.sql
@@ -17,4 +17,4 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 -- WALK RATINGS (walker1 and walker2 rated by owner1)
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
 (1, 1, 3, 5, 'Great walk!'),
-(2, 2, 3, 4, 'Good, but a bit late.');
+(2, 2, 3, 4, 'Good, but a bit late.');
